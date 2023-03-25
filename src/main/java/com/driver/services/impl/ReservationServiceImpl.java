8f2891f1acd49da167edc3938a3aa28d767ba611
harshit7014696433 <41208017+harshit7014696433@users.noreply.github.com>
2023-03-25 18:19:32 +0530

    done

--- src/main/java/com/driver/services/impl/ReservationServiceImpl.java
+++ src/main/java/com/driver/services/impl/ReservationServiceImpl.java
@@ -9,6 +9,7 @@ import com.driver.services.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -21,8 +22,63 @@ public class ReservationServiceImpl implements ReservationService {
     ReservationRepository reservationRepository3;
     @Autowired
     ParkingLotRepository parkingLotRepository3;
+
     @Override
     public Reservation reserveSpot(Integer userId, Integer parkingLotId, Integer timeInHours, Integer numberOfWheels) throws Exception {
+        User user;
+        ParkingLot parkingLot;
+        Reservation reservation = new Reservation();
+        reservation.setNumberOfHours(timeInHours);
 
+        try {
+            user = userRepository3.findById(userId).get();
+            parkingLot = parkingLotRepository3.findById(parkingLotId).get();
+        } catch (Exception e) {
+            throw new Exception("Cannot make reservation");
+        }
+        reservation.setUser(user);
+
+        user.getReservationList().add(reservation);
+
+        List<Spot> spotList = parkingLot.getSpotList();
+        List<Spot> notOccupied = new ArrayList<>();
+        for (Spot spot1 : spotList) {
+            if (spot1.getOccupied() == false) {
+                notOccupied.add(spot1);
+            }
+        }
+        if (notOccupied.isEmpty()) {
+            throw new Exception("Cannot make reservation");
+        }
+        Spot spot = null;
+        int mini = Integer.MAX_VALUE;
+        for (Spot spot1 : notOccupied) {
+            int value = spot1.getPricePerHour();
+            if(value<mini)
+            {
+                mini = value;
+                spot = spot1;
+            }
+        }
+
+        if(numberOfWheels>4){
+            if(spot.getSpotType().equals(SpotType.OTHERS))
+            {
+                reservation.setSpot(spot);
+            }
+        }
+        else if(numberOfWheels>2){
+            if(spot.getSpotType().equals(SpotType.FOUR_WHEELER))
+            {
+                reservation.setSpot(spot);
+            }
+
+        }
+        else spot.setSpotType(SpotType.TWO_WHEELER);
+
+
+        spot.getReservationList().add(reservation);
+        return reservation;
     }
+
 }
